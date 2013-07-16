4a6026d6e46e189c10592cf4314e0a6a43660235
emivulpe <emivulpe@abv.bg>
2013-07-16 18:28:17 +0300

    time stamping?

--- beginning/src/uk/ac/glasgow/etparser/handlers/SimulatedHeap.java
+++ beginning/src/uk/ac/glasgow/etparser/handlers/SimulatedHeap.java
@@ -1,4 +1,5 @@
 package uk.ac.glasgow.etparser.handlers;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,46 +9,61 @@ import uk.ac.glasgow.etparser.events.CreationEvent;
 import uk.ac.glasgow.etparser.events.Event;
 
 /**
+ * This class simulates the heap and keeps track of the object ids and the last
+ * event that happened to them as well as the total objects tried to be accessed
+ * ever.
  * 
- * @author Emi This class simulates the heap and keeps track of the object ids
- *         and the last event that happened to them as well as the total objects
- *         tried to be accessed ever
+ * @author Emi
+ * @version 1.0
+ * 
  * 
  */
 
 public class SimulatedHeap implements EventHandler {
-/**
- * This is analogous to the real heap.
- */
+	/**
+	 * This is analogous to the real heap memory.
+	 */
 	private static SimulatedHeap theHeap = null;
 	/**
 	 * Measures time sequentially (1, 2, 3...).
 	 */
-	private int timeSequence; 
+	private int timeSequence;
 	/**
 	 * Measures time as function of the allocated objects' sizes.
 	 */
 	private int timeSize;
+
 	/**
-	 * A hash map that keeps the object id and the last event 
-	 * that happened to that object.
+	 * Measures time in terms of method entry and method exit.
+	 */
+	private int timeMethod;
+	/**
+	 * A hash map that keeps the object id and the last event that happened to
+	 * that object.
 	 */
 	private HashMap<String, ObjectEventRecord> objectStates;
 	/**
-	 * A set of all the objects tried to be accessed ever
-	 * including the once that were not born.
+	 * A set of all the objects tried to be accessed ever including the once
+	 * that were not born.
 	 */
 	private Set<String> processedObjects;
-	
-    /**
-     * Initializes the class variables. Private because of Singleton design pattern.
-     */
+
+	/**
+	 * Initializes the class variables. Private because of Singleton design
+	 * pattern.
+	 */
 	private SimulatedHeap() {
 		timeSequence = 0;
+		timeSize = 0;
+		timeMethod = 0;
 		objectStates = new HashMap<String, ObjectEventRecord>();
 		processedObjects = new HashSet<String>();
 	}
-    
+
+	/**
+	 * 
+	 * @return an instance of the heap. It is global.
+	 */
 	public static SimulatedHeap getTheHeap() {
 		if (theHeap == null) {
 			theHeap = new SimulatedHeap();
@@ -56,6 +72,12 @@ public class SimulatedHeap implements EventHandler {
 		return theHeap;
 	}
 
+	/**
+	 * Checks whether the event is legal and if it is, it updates the event
+	 * record in the heap and updates the time.
+	 * @param e
+	 *            instance of the Event super class
+	 */
 	@Override
 	public void handle(Event e) {
 		processedObjects.add(e.getObjectID());
@@ -71,8 +93,8 @@ public class SimulatedHeap implements EventHandler {
 			ObjectEventRecord record = new ObjectEventRecord(e);
 			objectStates.put(currentObjectID, record);
 			e.setCheck("creation");
-			CreationEvent ce=(CreationEvent) e;
-			timeSize+=ce.getSize();
+			CreationEvent ce = (CreationEvent) e;
+			timeSize += ce.getSize();
 
 		}
 
@@ -81,6 +103,7 @@ public class SimulatedHeap implements EventHandler {
 
 			e.setCheck("not born");
 
+			// dead
 		} else if (existsInHeap && !objectStates.get(currentObjectID).isAlive()) {
 
 			e.setCheck("dead");
@@ -94,6 +117,10 @@ public class SimulatedHeap implements EventHandler {
 			objectStates.put(currentObjectID, record);
 			e.setCheck("legal");
 			System.out.println(objectStates.get(currentObjectID).isAlive());
+			if (currentEventStatus.equalsIgnoreCase("M")
+					|| currentEventStatus.equalsIgnoreCase("E")) {
+				timeMethod++;
+			}
 
 		}
 
@@ -102,22 +129,48 @@ public class SimulatedHeap implements EventHandler {
 			e.setCheck("created");
 
 		}
-		System.out.println(getNumObjects()+" total objects in heap"+processedObjects);
+		System.out.println(getNumObjects() + " total objects in heap"
+				+ processedObjects);
 	}
 
+	/**
+	 * 
+	 * @return the nmber of objects tried to be accessed ever.
+	 */
 	public int getNumObjects() {
 		return processedObjects.size();
 	}
 
+	/**
+	 * 
+	 * @param oid the id of the object we want to access in the heap.
+	 * @return the last event record for the given object.
+	 */
 	public ObjectEventRecord getRecord(String oid) {
 		return objectStates.get(oid);
 	}
-
-	public int getTimeSequence(){
+	
+	/**
+	 * 
+	 * @return the current time expressed as sequence.
+	 */
+	public int getTimeSequence() {
 		return timeSequence;
 	}
-	
-	public int getTimeSize(){
+
+	/**
+	 * 
+	 * @return the current time expressed as allocated object size.
+	 */
+	public int getTimeSize() {
 		return timeSize;
+	}
+
+	/**
+	 * 
+	 * @return  the current time expressed in term of method entry and exit.
+	 */
+	public int getTimeMethod() {
+		return timeMethod;
 	}
 }
